ff3b6412a9fca961b2f59b2681fbcaa1f7ea48b2
Mykola Ivashkovets <nivashkovets@gmail.com>
2026-09-21 15:24:52 +0300

    Add product service

---
+++ ViraelStudio.Application/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using ViraelStudio.Application.DTOs;
+
+namespace ViraelStudio.Application.Services
+{
+    public interface IProductService
+    {
+        Task<ProductDTO> CreateAsync(CreateProductDTO dto); 
+    }
+}
---
+++ ViraelStudio.Application/Services/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ViraelStudio.Application.DTOs;
+using ViraelStudio.Application.Mappings;
+
+namespace ViraelStudio.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        public Task<ProductDTO> CreateAsync(CreateProductDTO dto)
+        {
+            var product = dto.ToEntity();
+            //safe to DB
+            var result = product.ToDto();
+            return Task.FromResult(result);
+        }
+    }
+}
