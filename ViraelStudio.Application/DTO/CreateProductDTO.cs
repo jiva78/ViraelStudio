2a8f6344ed1b6c7675788d613878145745d1d498
Mykola Ivashkovets <nivashkovets@gmail.com>
2026-09-21 11:47:05 +0300

    Add product DTOs

--- ViraelStudio.Application/DTO/CreateProductDTO.cs
+++ ViraelStudio.Application/DTO/CreateProductDTO.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using ViraelStudio.Domain.Enums;
 
-namespace ViraelStudio.Domain.Entities
+namespace ViraelStudio.Application.DTO
 {
-    public class Product
+    public class CreateProductDTO
     {
-        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public int Quantity {  get; set; }
         public ProductType ProductType { get; set; }
-        public string? ImageUrl { get; set; }
+        public string? ImageUrl {  get; set; }
+        public int Quantity {  get; set; }
     }
 }
--- ViraelStudio.Application/DTO/ProductDTO.cs
+++ ViraelStudio.Application/DTO/ProductDTO.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using ViraelStudio.Domain.Enums;
 
-namespace ViraelStudio.Domain.Entities
+namespace ViraelStudio.Application.DTO
 {
-    public class Product
+    public class ProductDTO
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public ProductType ProductType { get; set; }    
+        public string? ImageUrl {  get; set; }
         public int Quantity {  get; set; }
-        public ProductType ProductType { get; set; }
-        public string? ImageUrl { get; set; }
     }
 }
