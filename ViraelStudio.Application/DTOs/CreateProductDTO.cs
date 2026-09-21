4b5105acf4ad4370e645fb0bce6667e3ceb57f5e
Mykola Ivashkovets <nivashkovets@gmail.com>
2026-09-21 12:13:11 +0300

    Add product mappings

--- ViraelStudio.Application/DTOs/CreateProductDTO.cs
+++ ViraelStudio.Application/DTOs/CreateProductDTO.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using ViraelStudio.Domain.Enums;
 
-namespace ViraelStudio.Application.DTO
+namespace ViraelStudio.Application.DTOs
 {
-    public class ProductDTO
+    public class CreateProductDTO
     {
-        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public ProductType ProductType { get; set; }    
+        public ProductType ProductType { get; set; }
         public string? ImageUrl {  get; set; }
         public int Quantity {  get; set; }
     }
---
+++ ViraelStudio.Application/Mappings/ProductMapping.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ViraelStudio.Application.DTOs;
+using ViraelStudio.Domain.Entities;
+
+namespace ViraelStudio.Application.Mappings
+{
+    public static class ProductMapping
+    {
+        public static Product ToEntity(this CreateProductDTO dto) 
+        {
+            return new Product
+            {
+                Name = dto.Name,
+                Description = dto.Description,
+                Price = dto.Price,
+                ProductType = dto.ProductType,
+                ImageUrl = dto.ImageUrl,
+                Quantity = dto.Quantity
+            };
+        }
+
+        public static ProductDTO ToDto(this Product product)
+        {
+            return new ProductDTO
+            { 
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                ProductType = product.ProductType,
+                ImageUrl = product.ImageUrl,
+                Quantity = product.Quantity
+            };
+        }
+    }
+}
